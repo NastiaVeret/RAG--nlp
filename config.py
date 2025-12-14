f1b288212a6926ab7a1953b4f6e00a2caf80ecef
Spoilsport <nastasiyka.ver@gmail.com>
2025-12-14 18:51:59 +0200

    added config file

---
+++ config.py
@@ -0,0 +1,8 @@
+
+
+DATASET_PATH = "dataset.docx"
+CHUNKS_FILE_PATH = "chunks_with_metadata.json"
+EMBEDDING_MODEL_NAME = 'all-MiniLM-L6-v2'
+RERANKER_MODEL_NAME = 'cross-encoder/ms-marco-MiniLM-L-6-v2'
+CHUNK_SIZE = 1000
+CHUNK_OVERLAP = 200
